b3915f0bd04f6c3929e6423c9df6dc0e6052bde8
vikalp <avikalp233@gmail.com>
2024-02-09 17:27:59 +0530

    Card actions

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -7,6 +7,8 @@ import { formatDistanceToNow } from "date-fns"
 import { useAuth } from "@clerk/nextjs"
 import { Footer } from "./footer"
 import { Skeleton } from "@/components/ui/skeleton"
+import { Actions } from "@/components/actions"
+import { MoreHorizontal } from "lucide-react"
 
 interface BoardCardProps {
     id: string,
@@ -49,6 +51,18 @@ export const BoardCard = ({
                     />
 
                     <Overlay />
+                    <Actions 
+                        id={id}
+                        title={title}
+                        side="right"
+
+                    >
+                        <button
+                            className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none"
+                        >
+                            <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity"/>
+                        </button>
+                    </Actions>
                 </div>
 
                 <Footer 
