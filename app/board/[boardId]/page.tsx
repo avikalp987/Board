6337bf6ba51385a19f93bcce25f568c1286b005a
vikalp <avikalp233@gmail.com>
2024-02-10 16:29:34 +0530

    Liveblocks setup

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,4 +1,6 @@
+import { Room } from "@/components/room"
 import { Canvas } from "./_components/canvas"
+import { Loading } from "./_components/loading"
 
 
 interface BoardIdPageProps {
@@ -11,9 +13,14 @@ const BoardIdPage = ({
     params,
 }: BoardIdPageProps) => {
     return (
-        <Canvas 
-            boardId={params.boardId}
-        />
+        <Room
+            roomId={params.boardId}
+            fallback={<Loading />}
+        >
+            <Canvas 
+                boardId={params.boardId}
+            />
+        </Room>
     )
 }
 
---
+++ components/room.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { RoomProvider } from "@/liveblocks.config"
+import { ReactNode } from "react"
+
+import { ClientSideSuspense } from "@liveblocks/react" //this is an alias
+
+interface RoomProps {
+    children: ReactNode,
+    roomId: string,
+    fallback: NonNullable<ReactNode> | null
+}
+
+
+export const Room = ({
+    children,
+    roomId,
+    fallback,
+}: RoomProps) => {
+    return (
+        <RoomProvider
+            id={roomId}
+            initialPresence={{}}
+        >
+            <ClientSideSuspense
+                fallback={fallback}
+            >
+                {() => children}
+            </ClientSideSuspense>
+        </RoomProvider>
+    )
+}
