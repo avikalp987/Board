ba7090ff369f3423c6a6ad8dcbf3f03577a0077b
vikalp <avikalp233@gmail.com>
2024-02-10 15:24:04 +0530

    Canvas layout

---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,20 @@
+import { Canvas } from "./_components/canvas"
+
+
+interface BoardIdPageProps {
+    params: {
+        boardId: string
+    }
+}
+
+const BoardIdPage = ({
+    params,
+}: BoardIdPageProps) => {
+    return (
+        <Canvas 
+            boardId={params.boardId}
+        />
+    )
+}
+
+export default BoardIdPage
