ffd0585950c3ae51a61c4278e4efd01f3b77294d
vikalp <avikalp233@gmail.com>
2024-02-10 15:01:32 +0530

    Search querying

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -4,6 +4,7 @@ import { api } from "@/convex/_generated/api"
 import { useApiMutation } from "@/hooks/use-api-mutation"
 import { cn } from "@/lib/utils"
 import { Plus } from "lucide-react"
+import { useRouter } from "next/navigation"
 import { toast } from "sonner"
 
 interface NewBoardButtonProps {
@@ -18,6 +19,8 @@ export const NewBoardButton = ({
 
     const { mutate, pending } = useApiMutation(api.board.create)
 
+    const router = useRouter()
+
     const onClick = () => {
         mutate({
             orgId,
@@ -25,7 +28,7 @@ export const NewBoardButton = ({
         })
         .then((id) => {
             toast.success("Board created")
-            //TODO: Redirect to /board/${id}
+            router.push(`/board/${id}`)
         })
         .catch(() => {
             toast.error("Failed to create board")
--- convex/board.ts
+++ convex/board.ts
@@ -55,7 +55,21 @@ export const remove = mutation({
             throw new Error("Unauthorized")
         }
 
-        //TODO: Later check to delete favorite relation as well
+        const userId = identity.subject
+
+        const existingFavorite = await ctx.db
+            .query("userFavorites")
+            .withIndex("by_user_board", (q) => 
+                q
+                    .eq("userId", userId)
+                    .eq("boardId", args.id)
+            )
+            .unique()
+
+        if(existingFavorite)
+        {
+            await ctx.db.delete(existingFavorite._id)
+        }
 
         await ctx.db.delete(args.id)
     }
@@ -119,11 +133,10 @@ export const favorite = mutation({
 
         const existingFavorite = await ctx.db
             .query("userFavorites")
-            .withIndex("by_user_board_org", (q) => 
+            .withIndex("by_user_board", (q) => 
                 q
                     .eq("userId", userId)
                     .eq("boardId", board._id)
-                    .eq("orgId", args.orgId)
             )
             .unique()
 
